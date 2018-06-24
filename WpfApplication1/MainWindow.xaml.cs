ba677e49561e906ec1aee2268ea04fded6390d2e
dma-PC\dma <jqirewtj@gkopfkmp.jnksdv>
2018-06-24 18:27:23 +0800

    期末

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      public int CA
+        {
+            get
+            {
+                try
+                {
+                    return int.Parse(CACACA.Text);
+                }
+
+                catch
+                {
+                    MessageBox.Show("卡洛里要輸入數字喔!!");
+                    return 0;
+                }
+                }
+            set
+            {
+                CACACA.Text = value.ToString();
+            }
+        }
+
+      public string FU
+        {
+            get
+            {
+                return Food.Text;
+            }
+            set
+            {
+                Food.Text = value.ToString();
+            }
+        }
+
+       
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+
+                int a = 0;
+                int b = int.Parse(gogle.Text);
+                a += CA;
+                Totle.Text = a.ToString();
+                if (b <a)
+                {
+                    MessageBox.Show(FU.ToString() + "" + CA.ToString() + "你要減肥囉");
+                }
+                else
+                {
+                    MessageBox.Show(FU + " " + CA + "繼續保持,好身材會是你的");
+                }
+            }
+        }
+    }
+}
---
+++ 期末/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CaCa_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Lady_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CACACACA_Click(object sender, RoutedEventArgs e)
+        {
+            int A;
+        }
+
+        private void Hello_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
